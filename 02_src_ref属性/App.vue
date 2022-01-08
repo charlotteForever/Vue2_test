deab3c26820d6aa78db61100c2eb32b14d84f798
charlotteForever <fernweh1001@163.com>
2022-01-08 17:34:16 +0800

    update

---
+++ 02_src_ref属性/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1 v-text="msg" ref="myDom"></h1>
+    <button @click="showDom">点击输出上方的DOM元素</button>
+    <School ref="sch" />
+  </div>
+</template>
+
+<script>
+import School from "./components/School.vue";
+export default {
+  data() {
+    return {
+      msg: "你好啊！",
+    };
+  },
+  components: {
+    School,
+  },
+  methods: {
+    showDom() {
+      //输出App组件的实例对象
+      console.log(this);
+      // 输出当前的Dom元素
+      console.log(this.$refs.myDom);
+      // 绑定在组件实例对象上是，获取到当前组件实例
+      console.log(this.$refs.sch);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <template>
   <div>
-    <h1 v-text="msg" ref="myDom"></h1>
-    <button @click="showDom">点击输出上方的DOM元素</button>
-    <School ref="sch" />
+    <h1 v-text="msg"></h1>
+    <!-- 这里的参数会传入props里，座位该组件实例对应的参数 -->
+    <!-- age前面加冒号数据绑定，是v-bind的缩写，冒号号里面成为表达式 -->
+    <school name="小明" :age="18 + 1" sex="male"></school>
   </div>
 </template>
 
 <script>
-import School from "./components/School.vue";
+import School from "./components/Student.vue";
 export default {
   data() {
     return {
-      msg: "你好啊！",
+      msg: "我们是尚硅谷的学生",
     };
   },
   components: {
     School,
-  },
-  methods: {
-    showDom() {
-      //输出App组件的实例对象
-      console.log(this);
-      // 输出当前的Dom元素
-      console.log(this.$refs.myDom);
-      // 绑定在组件实例对象上是，获取到当前组件实例
-      console.log(this.$refs.sch);
-    },
   },
 };
 </script>
