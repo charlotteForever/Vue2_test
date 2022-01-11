b075d23c6280454f89c5208f01d207e79ed927e7
charlotteForever <fernweh1001@163.com>
2022-01-11 22:31:11 +0800

    静态组件

--- 05_src_插件/main.js
+++ 05_src_插件/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import plugins from './plugins'
 
 Vue.config.productionTip = false
 
 // 需要导入模块后use
+Vue.use(plugins)
 new Vue({
     // render传入一个函数h，h函数传入App父组件，
     // render代替了template
