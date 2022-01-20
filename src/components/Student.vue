a74e5ed53b099684b9bd30cfbb1a1d98e0a1af44
charlotteForever <fernweh1001@163.com>
2022-01-20 21:14:21 +0800

    自定义事件基本使用

---
+++ src/components/Student.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="student">
+    <h3>学生姓名：{{ name }}</h3>
+    <h3>学生年龄：{{ age }}</h3>
+    <button @click="retName">点我得到学生姓名</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "花椰菜",
+      age: 19,
+    };
+  },
+  methods: {
+    // 在click触发的事件里触发student的自定义事件
+    retName() {
+      // 触发事件：atguigu，传入参数this.name
+      this.$emit("atguigu", this.name, 666, 888);
+    },
+  },
+};
+</script>
+
+<style>
+.student {
+  height: 150px;
+  width: 450px;
+  background-color: orange;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-// 需要导入模块后use
 new Vue({
-    // render传入一个函数h，h函数传入App父组件，
-    // render代替了template
     render: h => h(App)
 }).$mount('#app')
