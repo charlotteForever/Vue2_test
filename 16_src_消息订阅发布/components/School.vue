a539666225460f1ecac9c04076b05366adb6d6cd
charlotteForever <fernweh1001@163.com>
2022-02-06 21:12:40 +0800

    消息订阅发布

---
+++ 16_src_消息订阅发布/components/School.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="school">
+    <h3>学校名称：{{ name }}</h3>
+    <h3>学校地址：{{ address }}</h3>
+  </div>
+</template>
+
+<script>
+import pubsub from "pubsub-js";
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "尚硅谷",
+      address: "北京",
+    };
+  },
+  mounted() {
+    this.pid = pubsub.subscribe("getStuName", (name, data) => {
+      console.log(data);
+    });
+  },
+  // 取消订阅事件
+  beforeDestroy() {
+    pubsub.unsubscribe(this.pid);
+  },
+};
+</script>
+
+<style>
+.school {
+  height: 150px;
+  width: 450px;
+  background-color: pink;
+}
+</style>
---
+++ 16_src_消息订阅发布/components/Student.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="student">
+    <h3>学生姓名：{{ name }}</h3>
+    <h3>学生年龄：{{ age }}</h3>
+  </div>
+</template>
+
+<script>
+import pubsub from "pubsub-js";
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "花椰菜",
+      age: 19,
+    };
+  },
+  mounted() {
+    pubsub.publish("getStuName", this.name);
+  },
+};
+</script>
+
+<style>
+.student {
+  height: 150px;
+  width: 450px;
+  background-color: orange;
+}
+</style>
