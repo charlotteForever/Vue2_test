7a3a9df99109f76c6e2b771e60ecb2be98358523
charlotteForever <fernweh1001@163.com>
2022-02-06 11:02:20 +0800

    自定义事件总结

---
+++ 12_src_自定义事件总结/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app">
+    <h3>学生的名字是：{{ stuName }}</h3>
+    <School :getSchoolName="getSchoolName" />
+    <Student ref="student" />
+  </div>
+</template>
+
+<script>
+import School from "./components/School.vue";
+import Student from "./components/Student.vue";
+export default {
+  components: {
+    School,
+    Student,
+  },
+  data() {
+    return {
+      stuName: "",
+    };
+  },
+  methods: {
+    getSchoolName(name) {
+      console.log("app得到了学校的名称：" + name);
+    },
+    // 有多个参数时可以使用剩余参数
+    getStudentName(name) {
+      console.log("app得到了学生的姓名：" + name);
+      // vue的承诺：methods里面的this指向为当前组件的实例对象
+      this.stuName = name;
+    },
+  },
+  mounted() {
+    this.$refs.student.$on("atguigu", this.getStudentName);
+  },
+};
+</script>
+
+<style>
+.app {
+  height: 360px;
+  width: 500px;
+  background-color: grey;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,116 @@
 <template>
-  <div class="app">
-    <h3>学生的名字是：{{ stuName }}</h3>
-    <School :getSchoolName="getSchoolName" />
-    <Student ref="student" />
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <Add :addItem="addItem" />
+      <List :items="items" :checkItem="checkItem" :deleteItem="deleteItem" />
+      <Choice
+        :items="items"
+        :changeDone="changeDone"
+        :deleteDone="deleteDone"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import School from "./components/School.vue";
-import Student from "./components/Student.vue";
+import Add from "./components/Add.vue";
+import List from "./components/List.vue";
+import Choice from "./components/Choice.vue";
 export default {
   components: {
-    School,
-    Student,
+    Add,
+    List,
+    Choice,
   },
+  // 数据通过props传给List,List通过props传递给Item
   data() {
     return {
-      stuName: "",
+      items: [
+        { id: "001", name: "吃饭", done: false },
+        { id: "002", name: "睡觉", done: true },
+        { id: "003", name: "开车", done: false },
+      ],
     };
   },
   methods: {
-    getSchoolName(name) {
-      console.log("app得到了学校的名称：" + name);
+    // 添加一个item项
+    addItem(itemObj) {
+      this.items.unshift(itemObj);
     },
-    // 有多个参数时可以使用剩余参数
-    getStudentName(name) {
-      console.log("app得到了学生的姓名：" + name);
-      // vue的承诺：methods里面的this指向为当前组件的实例对象
-      this.stuName = name;
+    // 修改item的done状态
+    checkItem(id) {
+      // 数据在哪，就在哪里修改数据
+      // 遍历拿到对应的itemObj，修改done属性
+      this.items.forEach((todo) => {
+        if (todo.id === id) {
+          todo.done = !todo.done;
+        }
+      });
     },
-  },
-  mounted() {
-    this.$refs.student.$on("atguigu", this.getStudentName);
+    // 删除一个item
+    deleteItem(id) {
+      // filter返回一个新数组，所以需要再次赋值
+      this.items = this.items.filter((item) => {
+        return item.id != id;
+      });
+    },
+    changeDone(done) {
+      this.items.forEach((item) => {
+        item.done = done;
+      });
+    },
+    deleteDone() {
+      this.items = this.items.filter((item) => {
+        // 返回done为0，即没有勾选的 新数组
+        return !item.done;
+      });
+    },
   },
 };
 </script>
 
 <style>
-.app {
-  height: 360px;
-  width: 500px;
-  background-color: grey;
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
 }
 </style>
