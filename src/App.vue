31f181ccf6ce872791a062dc15bc9079fc2c410b
charlotteForever <fernweh1001@163.com>
2022-02-12 12:54:52 +0800

    "第三方动画库animate.css“

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div>
     <test />
     <test2 />
+    <test3 />
   </div>
 </template>
 
 <script>
 import test from "./components/test.vue";
 import test2 from "./components/test2.vue";
+import test3 from "./components/test3.vue";
 export default {
   components: {
     test,
     test2,
+    test3,
   },
 };
 </script>
---
+++ src/components/test3.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition
+      appear
+      name="animate__animated animate__bounce"
+      enter-active-class="animate__bounceInDown"
+      leave-active-class="animate__bounceOutDown"
+    >
+      <h1 v-show="isShow">你好啊！</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+import "animate.css";
+export default {
+  name: "test3",
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background: orange;
+}
+</style>
