2de50eb85faee887ab208b5a918b77bf5b5dfbce
charlotteForever <fernweh1001@163.com>
2022-02-16 15:12:42 +0800

    “默认插槽”

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <Category title="食物">
+      <img src="https://s3.ax1x.com/2021/01/16/srJlq0.jpg" alt="" />
+    </Category>
+    <Category title="游戏">
+      <ul>
+        <li v-for="(game, index) in games" :key="index">{{ game }}</li>
+      </ul>
+    </Category>
+    <Category title="电影">
+      <video
+        controls
+        src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
+      ></video>
+    </Category>
+  </div>
+</template>
+
+<script>
+import Category from "./components/category.vue";
+export default {
+  name: "App",
+  components: { Category },
+  data() {
+    return {
+      foods: ["火锅", "烧烤", "小龙虾", "牛排"],
+      games: ["红色警戒", "穿越火线", "劲舞团", "超级玛丽"],
+      films: ["《教父》", "《拆弹专家》", "《你好，李焕英》", "《尚硅谷》"],
+    };
+  },
+};
+</script>
+
+<style lang="css">
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+.category {
+  background: skyblue;
+  width: 200px;
+  height: 300px;
+}
+h3 {
+  background: orange;
+}
+img {
+  width: 100%;
+}
+video {
+  width: 100%;
+}
+</style>
---
+++ src/components/category.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}</h3>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: ["title"],
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
