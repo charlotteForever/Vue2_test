3d8e8111a26ce659c3b3810b5dc09730e058904f
charlotteForever <fernweh1001@163.com>
2022-02-17 13:57:02 +0800

    “求和案例纯vue"

---
+++ src/components/count.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h2>当前求和为：{{ sum }}</h2>
+    <select v-model.number="n">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click="increase">+</button>
+    <button @click="decrease">-</button>
+    <button @click="increaseOdd">当前求和为奇数再加</button>
+    <button @click="increaseWait">等一等再加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Count",
+  data() {
+    return {
+      sum: 0,
+      //  用户选择的数字
+      n: 1,
+    };
+  },
+  methods: {
+    increase() {
+      this.sum += this.n;
+    },
+    decrease() {
+      this.sum -= this.n;
+    },
+    increaseOdd() {
+      if (this.sum % 2 !== 0) {
+        this.sum += this.n;
+      }
+    },
+    increaseWait() {
+      setTimeout(() => {
+        this.sum += this.n;
+      }, 2000);
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+button {
+  margin-left: 5px;
+}
+</style>
