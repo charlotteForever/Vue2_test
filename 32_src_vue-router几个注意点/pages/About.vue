2cfe0bf903f031539c91748fdc3da2e865f37a8c
charlotteForever <fernweh1001@163.com>
2022-02-21 14:30:42 +0800

    “vue-router几个注意点"

---
+++ 32_src_vue-router几个注意点/pages/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>我是About的内容</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  mounted() {
+    console.log("about的", this.$route);
+    console.log("about的", this.$router);
+  },
+  beforeDestroy() {
+    console.log("About组件要被销毁了");
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 32_src_vue-router几个注意点/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>我是Home的内容</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  mounted() {
+    console.log("home的", this.$route);
+    console.log("home的", this.$router);
+  },
+  beforeDestroy() {
+    console.log("Home组件要被销毁了");
+  },
+};
+</script>
+
+<style>
+</style>
