ea2aeed42b02118f3863eee905667b21e7c19668
charlotteForever <fernweh1001@163.com>
2022-02-24 23:01:35 +0800

    "编程式路由导航"

---
+++ 38_src_编程式路由导航/pages/Detial.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ul>
+      <!-- 如果要接受的参数很多,这样写就很麻烦 -->
+      <!-- <li>消息编号：{{ $route.params.id }}</li>
+      <li>消息标题：{{ $route.params.title }}</li> -->
+      <li>消息编号：{{ id }}</li>
+      <li>消息标题：{{ title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Detial",
+  props: ["id", "title"],
+  mounted() {
+    console.log("detial", this);
+  },
+};
+</script>
+
+<style>
+</style>
