33f4a03b994d675b2825eb859ad5c7b55aca40d6
charlotteForever <fernweh1001@163.com>
2022-02-24 12:07:20 +0800

    "props"

---
+++ 37_src_props配置项/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-xs-offset-2 col-xs-8">
+        <div class="page-header">
+          <h2>
+            <Banner />
+          </h2>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-2 col-xs-offset-2">
+        <div class="list-group">
+          <!-- <a class="list-group-item active" href="./about.html">About</a> -->
+          <!-- <a class="list-group-item" href="./home.html">Home</a> -->
+          <router-link
+            class="list-group-item"
+            active-class=" active"
+            :to="{ name: 'guanyu' }"
+            >About</router-link
+          >
+          <router-link class="list-group-item" active-class="active" to="/home"
+            >Home</router-link
+          >
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="panel">
+          <div class="panel-body">
+            <!-- <h2>我是About的内容</h2> -->
+            <!-- 设置两个组件内容的展示位置 -->
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Banner from "./components/Banner.vue";
+export default {
+  name: "App",
+  components: {
+    Banner,
+  },
+  // 不需要注册子组件，因为不会以<About/>的方式使用子组件
+  // components: {
+  //   About,
+  //   Home,
+  // },
+};
+</script>
---
+++ 37_src_props配置项/pages/Message.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="m in messageList" :key="m.id">
+        <!-- 本来要放a标签，点击之后可以显示message的具体信息 -->
+        <!-- 跳转并携带query参数————字符串写法 -->
+        <!-- <router-link :to="`/home/message/detial?id=${m.id}&title=${m.title}`"> -->
+        <!-- 跳转并携带params参数————字符串写法 -->
+        <!-- <router-link :to="`/home/message/detial/${m.id}/${m.title}`">
+          {{ m.id }}
+        </router-link> -->
+        <!-- 可以不要path直接name -->
+        <router-link
+          :to="{
+            name: 'xijie',
+            params: {
+              id: m.id,
+              title: m.title,
+            },
+          }"
+        >
+          {{ m.id }}
+        </router-link>
+      </li>
+    </ul>
+    <hr />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  data() {
+    return {
+      messageList: [
+        { id: "001", title: "哈哈哈" },
+        { id: "002", title: "嘻嘻嘻" },
+        { id: "003", title: "吼吼吼" },
+      ],
+    };
+  },
+  mounted() {
+    console.log(this);
+  },
+};
+</script>
+
+<style>
+</style>
